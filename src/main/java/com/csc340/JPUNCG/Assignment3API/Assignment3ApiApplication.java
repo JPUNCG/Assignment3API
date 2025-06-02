efc20786697a6e4f970cb7f3357dd5f95e5713de
JPUNCG <j_pallinti@uncg.edu>
2025-06-02 16:51:17 -0400

    init

---
+++ src/main/java/com/csc340/JPUNCG/Assignment3API/Assignment3ApiApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.JPUNCG.Assignment3API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment3ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment3ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csc340/JPUNCG/Assignment3API/Assignment3ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc340.JPUNCG.Assignment3API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment3ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
